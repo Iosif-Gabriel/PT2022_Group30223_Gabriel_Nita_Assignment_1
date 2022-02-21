ec20a3d361c02d210e717e818cef8658c5c85c61
nitagabriel <gabriel_nita55@yahoo.com>
2022-02-21 11:36:56 +0200

    update

---
+++ src/main/java/Monomial.java
@@ -0,0 +1,20 @@
+public class Monomial {
+    private int grad;
+    private double coeficient;
+
+    public int getGrad() {
+        return grad;
+    }
+
+    public void setGrad(int grad) {
+        this.grad = grad;
+    }
+
+    public double getCoeficient() {
+        return coeficient;
+    }
+
+    public void setCoeficient(double coeficient) {
+        this.coeficient = coeficient;
+    }
+}
---
+++ src/main/java/Polinom.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Polinom {
+
+    private List<Monomial> poli=new ArrayList<>();
+}
